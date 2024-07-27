29e9bf993cc7cc9e6058a39a8aba9c07ceedc33f
nkosi-tauro <nkosilati23@yahoo.com>
2024-07-27 15:55:30 +0200

    Added Main Application Layout

---
+++ src/core/views/MainLayout.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useDashboardStore } from '../store/dashboard';
+import DashboardSideBar from './DashboardSideBar.vue';
+import DashboardTopNavigation from './DashboardTopNavigation.vue';
+
+</script>
+
+<template>
+  <div>
+    <!-- Dashboard Side Bar -->
+    <DashboardSideBar/>
+
+    <div class="lg:pl-72">
+      <!-- Dashboard Top Navigation -->
+      <DashboardTopNavigation/>
+
+      <main class="py-10">
+        <div class="px-4 sm:px-6 lg:px-8">
+          <!-- Main Content -->
+          <RouterView/>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
